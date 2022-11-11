1fc920af89096c87675fc02ca61457b5ac151bcb
GermanIvk05 <48442292+GermanIvk05@users.noreply.github.com>
2022-11-11 12:27:01 -0400

    Added new feautures to Audit Policy Changer
    Added backup functionality. Additionally, the audit policy change was missing applying changes functionality.

--- AuditPolicyChanger.bat
+++ AuditPolicyChanger.bat
@@ -1,5 +1,10 @@
 @echo off
 
+if exist "C:\audit.csv" del "C:\audit.csv"
+
+REM "Do backup of the original policies"
+auditpol /backup /file:"C:\audit.csv"
+
 REM "Audit: Force audit policy subcategory settings (Windows Vista or later) to override audit policy category settings."
 REG ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v SCENoApplyLegacyAuditPolicy /t REG_DWORD /d 1 /f
 
@@ -80,5 +85,17 @@ auditpol /set /subcategory:"Detailed File Share" /failure:enable
 
 REM "V-220791"
 auditpol /set /subcategory:"MPSSVC Rule-Level Policy Change" /success:enable /failure:enable
+
+if not exist "C:\Windows\System32\GroupPolicy\Machine\Microsoft\Windows NT\Audit" (
+	mkdir "C:\Windows\System32\GroupPolicy\Machine\Microsoft\Windows NT\Audit"
+)
+
+if exist "C:\Windows\System32\GroupPolicy\Machine\Microsoft\Windows NT\Audit\audit.csv" (
+	del "C:\Windows\System32\GroupPolicy\Machine\Microsoft\Windows NT\Audit\audit.csv"
+)
+
+auditpol /backup /file:"C:\Windows\System32\GroupPolicy\Machine\Microsoft\Windows NT\Audit\audit.csv"
+
+gpupdate /force
 
 PAUSE
